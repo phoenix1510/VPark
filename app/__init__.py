274e41a1bef068697b4b87de24bd35fa20d2f82e
pheonix1510 <soniharshil.1510@gmail.com>
2026-09-03 23:55:20 +0530

    optimize app factory

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,17 @@
 from flask import Flask 
 
 def create_app():
+    #create the app
     app = Flask(__name__)
 
+    #add configuration
     app.config.from_object('app.config.config.Config')
 
+    #register blueprints
     # from app.routes. import 
     # app.register_blueprint(auth_bp)
+
+    #register db teardown
+    from app.db.init_db import close_db
+    app.teardown_appcontext(close_db)
     return app
