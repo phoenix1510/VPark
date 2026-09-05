03c92adf3e7620e7cdc477296b281e7efce82b18
pheonix1510 <soniharshil.1510@gmail.com>
2026-09-05 21:54:02 +0530

    fix form classes to have submit field

--- app/routes/wtfforms.py
+++ app/routes/wtfforms.py
@@ -3,12 +3,14 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Length , Email , EqualTo
 
 class LoginForm(FlaskForm):
-    email = StringField('Username', validators=[DataRequired(), Email()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired(),Length(min=4, max=20)])
+    submit = SubmitField('Login')
 
-class SignupForm():
+class SignupForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
-    email = StringField('Username', validators=[DataRequired(), Email()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired(),Length(min=4, max=20),EqualTo('confirm',message='Passwords must match!')])
     confirm = PasswordField('Repeat Password')
     phone_number = StringField('Phone number', validators=[DataRequired(),Length(min=10, max=12)])
+    submit = SubmitField("Register")
